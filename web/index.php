df6f26fc8e155c0b8fc48fd222b853311ea933e1
Akhilesh Sharma <55455350+akhilesshharma@users.noreply.github.com>
2022-02-28 01:47:55 +0530

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ESCROW</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<h1 id="p-title">ESCROW</h1>
+	<div class="row">
+		<div class="column-1">
+			<div class="buyer-col">
+                                <h2>Buyer</h2>
+                                <p>Some text..</p>
+                        </div>
+		</div>
+
+		<div class="column-2" style="background-color:#bbb;">
+			<div class="seller-col">
+                                <h2>Seller</h2>
+                                <p>Some text..</p>
+                        <div>
+		</div>
+	</div>
+	
+</body>
+
+<script type="text/javascript">
+
+if (screen.width <= 699) {
+document.location = "../mob/";
+}
+
+</script>
+
+</html>
