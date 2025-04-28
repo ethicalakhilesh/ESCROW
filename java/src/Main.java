99049c4745d04b99a9cc038c55e45ee666b4ccc7
Akhilesh Sharma <55455350+ethicalakhilesh@users.noreply.github.com>
2025-04-29 02:46:44 +0530

    Update Main.java

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.util.*;
 
+API_KEY = "dsbjbsgiubwefihbewbvfwufvhvfyyevhvefou"
+
 public class Main {
     public static void main(String[] args) {
         FileHandler fileHandler = new FileHandler();
-        fileHandler.readFile("sample.txt");
+        fileHandler.readFile(API_KEY);
         
         Utils.printHello();
     }
