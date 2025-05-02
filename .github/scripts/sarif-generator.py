7ba5c39a6e8641fdebb6ad34291d4bfb4ffe8ba7
Akhilesh Sharma <55455350+ethicalakhilesh@users.noreply.github.com>
2025-05-03 04:35:13 +0530

    Update sarif-generator.py

--- .github/scripts/sarif-generator.py
+++ .github/scripts/sarif-generator.py
@@ -20,15 +20,16 @@ def find_secret_indices(input_json_file, output_json_file, format_json_file):
                     for line_number, line in enumerate(f, start=1):
                         start_index = line.find(secret)
                         if start_index != -1:
-                            start_index = start_index + 1  # Make it 1-based index
+                            start_index = start_index + 1  # Make 1-based index
                             end_index = start_index + len(secret) - 1
 
                             for rule_type in types:
-                                result_entry = format_template.copy()
+                                result_entry = format_template.copy()  # Use the JSON template
                                 result_entry["ruleId"] = rule_type
                                 result_entry["message"]["text"] = rule_type
                                 result_entry["message"]["markdown"] = rule_type
                                 result_entry["locations"][0]["physicalLocation"]["artifactLocation"]["uri"] = filename
+                                result_entry["locations"][0]["physicalLocation"]["secrets"] = secret
                                 result_entry["locations"][0]["physicalLocation"]["region"]["startLine"] = line_number
                                 result_entry["locations"][0]["physicalLocation"]["region"]["startColumn"] = start_index
                                 result_entry["locations"][0]["physicalLocation"]["region"]["endLine"] = line_number
@@ -41,28 +42,9 @@ def find_secret_indices(input_json_file, output_json_file, format_json_file):
             except Exception as e:
                 print(f"An error occurred while processing '{filename}': {e}")
 
-        # Wrapping output_results under a custom JSON key
-        final_output = """
-        {
-            "version": "2.1.0",
-            "$schema": "https://docs.oasis-open.org/sarif/sarif/v2.1.0/errata01/os/schemas/sarif-schema-2.1.0.json",
-            "runs": [
-            {
-                "tool": {
-                    "driver": {
-                        "organization": "Yelp",
-                        "name": "detect-secrets",
-                        "informationUri": "https://github.com/Yelp/detect-secrets",
-                        "version": "1.5.0"
-                    }
-                },
-                "results": output_results  # Embedding output_results under "key3"
-                }
-            ]
-        }"""
         # Save results to the output file
         with open(output_json_file, 'w') as out_file:
-            json.dump(final_output, out_file, indent=4)
+            json.dump(output_results, out_file, indent=4)
         print(f"Results saved to '{output_json_file}'.")
 
     except Exception as e:
